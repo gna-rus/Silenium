bffb1612ccc80038d7ccc7752735cc281925d095
gna-rus <gnarus@inbox.ru>
2023-12-15 15:36:51 +0300

    2_commit

--- Selenium_smoke_test.py
+++ Selenium_smoke_test.py
@@ -8,8 +8,8 @@ driver = webdriver.Chrome()
 base_url = 'https://www.saucedemo.com/'
 driver.get(base_url)
 
-standart_login = 'standard_user' # логин для входа
-password_all = 'secret_sauce' # пароль для входа
+standart_login = 'standard_user'  # логин для входа
+password_all = 'secret_sauce'  # пароль для входа
 
 driver.maximize_window()
 # user_name = driver.find_element(by=By.ID, value="user-name") # ID
@@ -35,38 +35,37 @@ button_login.click()
 print("Click Login Button")
 
 text_products = driver.find_element(by=By.XPATH, value="//*[@id='header_container']/div[2]/span")
-value_text_products = text_products.text # считываем текст из логотипа на сайте
+value_text_products = text_products.text  # считываем текст из логотипа на сайте
 print(f"Логотип страницы после авторизации: {value_text_products}")
-assert value_text_products == "Products" # проверка, на правильную ли страницу происходит перенос при авторизации по названию логотипа
+assert value_text_products == "Products"  # проверка, на правильную ли страницу происходит перенос при авторизации по названию логотипа
 
 get_url = driver.current_url
-real_url = "https://www.saucedemo.com/inventory.html" # Проверка. Нужный url после авторизации
+real_url = "https://www.saucedemo.com/inventory.html"  # Проверка. Нужный url после авторизации
 print(f"Перешли на страницу: {get_url}")
 assert get_url == real_url
 
 time.sleep(2)
 
 """Smoke Test #1"""
-product_1 = driver.find_element(by = By.XPATH, value = "//a[@id = 'item_4_title_link']")
+product_1 = driver.find_element(by=By.XPATH, value="//a[@id = 'item_4_title_link']")
 text_of_product_1 = product_1.text
 print(text_of_product_1)
 
-price_product_1 = driver.find_element(by = By.XPATH, value = "//*[@id='inventory_container']/div/div[1]/div[2]/div[2]/div")
+price_product_1 = driver.find_element(by=By.XPATH, value="//*[@id='inventory_container']/div/div[1]/div[2]/div[2]/div")
 text_of_price_product_1 = price_product_1.text
 print(text_of_price_product_1)
 
-buttom_product_1 = driver.find_element(by = By.XPATH, value = "//*[@id='add-to-cart-sauce-labs-backpack']")
+buttom_product_1 = driver.find_element(by=By.XPATH, value="//*[@id='add-to-cart-sauce-labs-backpack']")
 text_of_buttom_product_1 = buttom_product_1.text
 print(text_of_buttom_product_1)
 buttom_product_1.click()
 
-get_url_cart= "https://www.saucedemo.com/cart.html"
-cart = driver.find_element(by = By.XPATH, value = "//*[@id='shopping_cart_container']/a")
+get_url_cart = "https://www.saucedemo.com/cart.html"
+cart = driver.find_element(by=By.XPATH, value="//*[@id='shopping_cart_container']/a")
 text_of_cart = cart.text
 print(text_of_cart)
 cart.click()
 url_of_crunk = driver.current_url
 assert get_url_cart == url_of_crunk
-
 
 time.sleep(2)
